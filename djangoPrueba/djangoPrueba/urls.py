9842c7d9b5f444cae5aee75b5cdcbf7643308a0f
“xn1llox“ <felipeespinozaguajardo@gmail.com>
2022-09-25 21:12:19 -0300

    actualizado

---
+++ djangoPrueba/djangoPrueba/urls.py
@@ -0,0 +1,27 @@
+"""djangoPrueba URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from firstApp import views as app1
+from secondApp import views as app2
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("hola/", app1.display),
+    path("ahora/", app1.displayDatetime),
+    path("saludo/", app2.saludo),
+]
---
+++ djangoPrueba/firstApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+# Create your views here.
+def display(request):
+    return HttpResponse("<h1>Hola Mundo</h1>")
+
+def displayDatetime(request):
+    dt = datetime.datetime.now()
+    s = "<b>Fecha y hora catuales: </b>" + str(dt)
+    return HttpResponse(s)
